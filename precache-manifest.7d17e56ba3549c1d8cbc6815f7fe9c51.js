8e1a24b679350ee3078b3d53d460c0b55dec520c
cristian97r <cristian97drb@gmail.com>
2019-04-05 16:47:16 -0400

    Updates

---
+++ precache-manifest.7d17e56ba3549c1d8cbc6815f7fe9c51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41814b709377eccae4ea",
+    "url": "/meme-generator/static/css/main.814a380a.chunk.css"
+  },
+  {
+    "revision": "41814b709377eccae4ea",
+    "url": "/meme-generator/static/js/main.29ad7a66.chunk.js"
+  },
+  {
+    "revision": "b8372d4f76d85c66a91a",
+    "url": "/meme-generator/static/js/runtime~main.0d1394e5.js"
+  },
+  {
+    "revision": "10192006d9199411c6b6",
+    "url": "/meme-generator/static/js/2.b25348a8.chunk.js"
+  },
+  {
+    "revision": "3842adaf4b53a0846c6ca448a716c72c",
+    "url": "/meme-generator/index.html"
+  }
+];
